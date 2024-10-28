89f7b1645937d8758c7231e8f2fa0e1b4ed47eae
Robert Källgren <robertkallgren@gmail.com>
2024-10-28 15:46:27 +0100

    Basic electron + react + vite + typescript + tailwind setup
    Courtesy of Cursor
    
    Prompt: Create a new electron project following best practices using
    react, typescript, tailwind and vite. The app should be called "Bubbles"
    
    npm install @types/react @types/react-dom @types/node electron electron-is-dev react react-dom
    npm install -D @types/electron @vitejs/plugin-react autoprefixer electron-builder postcss tailwindcss typescript vite vite-plugin-electron

---
+++ src/main/main.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import isDev from "electron-is-dev";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
